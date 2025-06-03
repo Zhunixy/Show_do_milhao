a11fe31fc45f9880e1f85c2fab825758ba0c099a
dolphin4748 <matheusluciocx5726@gmail.com>
2025-06-03 14:32:02 -0300

    timer para cada questão

--- functions.py
+++ functions.py
@@ -1,18 +1,22 @@
 import time
 import os
+import sys
 
-def printar_menu(i, perguntas, pergunta_final, pontuacao = 0):
-    os.system("cls")
-    if pontuacao: print(f"pontuação atual: R${pontuacao}")
-    if pergunta_final: print(f"pergunta final. {perguntas[i][0]} | {perguntas[i][1]}") 
-    else: print(f"{i+1}. {perguntas[i][0]} | {perguntas[i][1]}")
-    print(f"A){perguntas[i][2]} \nB){perguntas[i][3]} \nC){perguntas[i][4]} \nD){perguntas[i][5]}")
-    print("•─────────★•♛•★────────• ")
-    print("1 -> responder")
-    print("2 -> auxilio")
-    print("3 -> pular")
-    print("0 -> desistir")
-    print("•─────────★•♛•★────────• ")
+def printar_menu(i, perguntas, pergunta_final, tempo, pontuacao = 0):
+    sys.stdout.write(f"\033[{100}A")
+
+    if pontuacao: sys.stdout.write(f"pontuação atual: R${pontuacao}                                             \n")
+    if pergunta_final: sys.stdout.write(f"pergunta final. {perguntas[i][0]} | {perguntas[i][1]}                 \n")
+    else: sys.stdout.write(f"{i+1}. {perguntas[i][0]} | {perguntas[i][1]}                                       \n")
+    sys.stdout.write(f"A){perguntas[i][2]} \nB){perguntas[i][3]} \nC){perguntas[i][4]} \nD){perguntas[i][5]}    \n")
+    sys.stdout.write("•─────────★•♛•★────────•                                                                \n")
+    sys.stdout.write("1 -> responder                                                                            \n")
+    sys.stdout.write("2 -> auxilio                                                                              \n")
+    sys.stdout.write("3 -> pular                                                                                \n")
+    sys.stdout.write("0 -> desistir                                                                             \n")
+    sys.stdout.write("•─────────★•♛•★────────•                                                                \n")
+    sys.stdout.write(f"timer: {int(tempo)}                                                                      \n")
+    sys.stdout.flush()
 
 def gera_delay(fps, tempo):
     fps = fps
--- main.py
+++ main.py
@@ -51,14 +51,20 @@ shuffle(perguntas_medios)
 shuffle(perguntas_dificeis)
 shuffle(perguntas_m_dificeis)
 
-fps = 1
+fps = 30
 tempo_por_frame = 1 / fps
+
+tempo = 45
 
 print("•─────────★•♛•★────────• ")
 print("Bem vindo ao Show do Pythão!")
 print("•─────────★•♛•★────────• ")
 
 while running and i < 17:
+    os.system("cls")
+
+    opcao = "a"
+    escolha = 0
 
     if i >= 0 and i < 5:
         perguntas = perguntas_faceis
@@ -71,54 +77,81 @@ while running and i < 17:
         perguntas = perguntas_m_dificeis
         pergunta_final = True
 
-    printar_menu(i, perguntas, pergunta_final, pontuacao)
     while True:   
         inicio = time.time()
 
+        printar_menu(i, perguntas, pergunta_final, tempo, pontuacao)
+        print(">>                                                 ")
+
+        tempo -= 1/30
+
+        if tempo < 1:
+            print("•─────────★•♛•★────────• ")
+            print("demorou demais para responder seu tempo acabou!")
+            print(f"sua pontuação final foi R${pontuacao}")
+            print("•─────────★•♛•★────────• ")
+            running = False
+            break
+
         try:
-            print(">>")
-            opcao = int(kb.read_event().name)
+            if kb.is_pressed("0"):
+                opcao = 0
+            if kb.is_pressed("1"):
+                opcao = 1
+            if kb.is_pressed("2"):
+                opcao = 2
+            if kb.is_pressed("3"):
+                opcao = 3
+            #opcao = int(kb.read_event().name)
         except:
-            printar_menu(i, perguntas, pergunta_final, pontuacao)
             continue
         match opcao:
             case 1:
-                gera_delay(6, 1)
-                print("resposta: ")
-                escolha = str(kb.read_event().name)
-                    
-                if escolha.upper() == perguntas[i][6].upper():
-                    if i >= 0 and i < 5:
-                        pontuacao += pontos[0]
-                    elif i >=5 and i < 10:
-                        if pontuacao < pontos[1]: pontuacao = pontos[1]
-                        else: pontuacao += pontos[1]
-                    elif i >= 10 and i < 15:
-                        if pontuacao < pontos[2]: pontuacao = pontos[2]
-                        else: pontuacao += pontos[2] 
-                    elif i == 15:
-                        pontuacao += pontos[3]
-                    if pergunta_final:
-                        running = False
-                        pontuacao = pontos[-1]
+                printar_menu(i, perguntas, pergunta_final, tempo, pontuacao)
+                print("resposta:                                          ")
 
-                    os.system("cls")
-                    print("•─────────★•♛•★────────• ")
-                    print("Parabéns! você acertou!")
-                    print("•─────────★•♛•★────────• ")
-                    gera_delay(1, 1)
-                    acertos += 1
-                    break 
-                else:
-                    pontuacao = int(pontuacao/2)
-                    print("•─────────★•♛•★────────• ")
-                    print("Você errou! 😂")
-                    print(f"sua pontuação final foi R${pontuacao}")
-                    print("•─────────★•♛•★────────• ")
-                    running = False
-                    break
+                if kb.is_pressed("a"):
+                    escolha = "a"
+                if kb.is_pressed("b"):
+                    escolha = "b"
+                if kb.is_pressed("c"):
+                    escolha = "c"
+                if kb.is_pressed("d"):
+                    escolha = "d"
+                
+                if escolha:
+                    if escolha.upper() == perguntas[i][6].upper():
+                        if i >= 0 and i < 5:
+                            pontuacao += pontos[0]
+                        elif i >=5 and i < 10:
+                            if pontuacao < pontos[1]: pontuacao = pontos[1]
+                            else: pontuacao += pontos[1]
+                        elif i >= 10 and i < 15:
+                            if pontuacao < pontos[2]: pontuacao = pontos[2]
+                            else: pontuacao += pontos[2] 
+                        elif i == 15:
+                            pontuacao += pontos[3]
+                        if pergunta_final:
+                            running = False
+                            pontuacao = pontos[-1]
+
+                        os.system("cls")
+                        print("•─────────★•♛•★────────• ")
+                        print("Parabéns! você acertou!")
+                        print("•─────────★•♛•★────────• ")
+                        gera_delay(1, 1)
+                        acertos += 1
+                        break 
+                    else:
+                        pontuacao = int(pontuacao/2)
+                        print("•─────────★•♛•★────────• ")
+                        print("Você errou! 😂")
+                        print(f"sua pontuação final foi R${pontuacao}")
+                        print("•─────────★•♛•★────────• ")
+                        running = False
+                        break
             case 2:
-                printar_menu(i, perguntas, pergunta_final, pontuacao)
+                printar_menu(i, perguntas, pergunta_final, tempo, pontuacao)
                 print(perguntas[i][7])
             case 3:
                 if pulos < 3:
@@ -133,26 +166,26 @@ while running and i < 17:
                         continue
                     break 
                 else:
-                    printar_menu(i, perguntas, pergunta_final, pontuacao)
+                    os.system("cls")
+                    printar_menu(i, perguntas, pergunta_final, tempo, pontuacao)
                     print("voce excedeu seu limite de pulos")
                     continue
             case 0:
                 print("Saindo do quiz...")
                 running = False
                 break
-            case _:
-                print("Opção inválida, digite 0, 1, 2 ou 3.")
-        
-    i += 1
 
-    duracao = time.time() - inicio
-    if duracao < tempo_por_frame:
-        time.sleep(tempo_por_frame - duracao)
+        duracao = time.time() - inicio
+        if duracao < tempo_por_frame:
+            time.sleep(tempo_por_frame - duracao)
+    
+    tempo = 0
+    i += 1
 
 print("•─────────★•♛•★────────• ")
 print("encerrando o show do pythão")
-print(f"sua pontuação foi de {pontuacao}")
+print(f"sua pontuação foi de R${pontuacao}")
 print("•─────────★•♛•★────────• ")
 
 with open("resultado.txt", "a+", encoding="utf-8") as arquivo:
-    arquivo.write(f"Você acertou {acertos} de 10 perguntas e teve como pontuação R${pontuacao}\n")
+    arquivo.write(f"Você acertou {acertos} de 17 perguntas e teve como pontuação R${pontuacao}\n")
